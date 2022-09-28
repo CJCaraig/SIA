aec376cfb3662dad0aed97c1327219fa75fe6982
CJCaraig <114575072+CJCaraig@users.noreply.github.com>
2022-09-28 17:55:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<p>ACTIVITY 1</p><br>
+<p>NAME: CRISTEL JOY CARAIG</p><br>
+<p>SECTION : BSIT-4A</p><br>
+<p>SUBJECT : SYSTEM INTEGRATION AND ARCHITECTURE</p><br>
+</body>
+</html>
